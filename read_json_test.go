a1f19f530b0a6aec8338e3e136f33fbfdc686683
Vladimir Ruzanov <nnovzver@gmail.com>
2015-07-14 14:18:34 +0300

    Simplify TestParseJsonModule

--- read_json_test.go
+++ read_json_test.go
@@ -26,21 +26,8 @@ func TestParseJsonModule(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	orig := Module{}
-	orig.Name = "Simple"
-	orig.Codograms = make([]Codogram, 1)
-	orig.Codograms[0].Name = "First"
-	orig.Codograms[0].Fields = make([]Field, 4)
-	orig.Codograms[0].Fields[0].Name = "i"
-	orig.Codograms[0].Fields[0].Length = 2
-	orig.Codograms[0].Fields[1].Name = "j"
-	orig.Codograms[0].Fields[1].Length = 7
-	orig.Codograms[0].Fields[2].Name = "k"
-	orig.Codograms[0].Fields[2].Length = 16
-	orig.Codograms[0].Fields[3].Name = "l"
-	orig.Codograms[0].Fields[3].Length = 32
 
-	if !reflect.DeepEqual(m, orig) {
+	if !reflect.DeepEqual(m, simpleModule) {
 		t.Error("m, orig not equal")
 	}
 }
