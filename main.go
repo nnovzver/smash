053d6b7700e1f60f3a0477696597994b0d08248d
Vladimir Ruzanov <nnovzver@gmail.com>
2015-07-16 17:01:31 +0300

    Add command flag to generate code to stdout

--- main.go
+++ main.go
@@ -7,15 +7,21 @@ import (
 	"strings"
 )
 
-func printUsage() {
-	fmt.Fprintf(os.Stderr, "Usage: %s json_module_filename\n", os.Args[0])
+var stdOut bool
+func init() {
+	flag.BoolVar(&stdOut, "s", false, "print generated code to stdout")
 }
 
 func main() {
+	flag.Usage = func() {
+        fmt.Fprintf(os.Stderr, "Usage: %s [flags] json_module_filename\n", os.Args[0])
+        flag.PrintDefaults()
+	}
+
 	flag.Parse()
 	args := flag.Args()
 	if len(args) != 1 {
-		printUsage()
+		flag.Usage()
 		return
 	}
 	filename := flag.Args()[0]
@@ -23,16 +29,21 @@ func main() {
 	_, err := os.Stat(filename)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "No such file or directory: %s\n", filename)
+		flag.Usage()
 		return
 	}
 
-	hfile, err := os.Create(strings.TrimSuffix(filename, "json") + "h")
-	if err != nil {
-		panic(err)
-	}
-	cfile, err := os.Create(strings.TrimSuffix(filename, "json") + "c")
-	if err != nil {
-		panic(err)
+	var hfile = os.Stdout
+	var cfile = os.Stdout
+	if !stdOut {
+		hfile, err = os.Create(strings.TrimSuffix(filename, "json") + "h")
+		if err != nil {
+			panic(err)
+		}
+		cfile, err = os.Create(strings.TrimSuffix(filename, "json") + "c")
+		if err != nil {
+			panic(err)
+		}
 	}
 	err = GenerateCFiles(filename, hfile, cfile)
 	if err != nil {
