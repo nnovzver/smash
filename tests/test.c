cba5a8f272b29e9114124ecb0bf3694753416743
Vladimir Ruzanov <nnovzver@gmail.com>
2015-07-21 16:33:38 +0300

    Fix codogram size errors in C test

--- tests/test.c
+++ tests/test.c
@@ -8,28 +8,28 @@ int main() {
   First c = {0, 0, 3, 4};
   First cc;
   First cc_etalon = {2, 4, 3, 4};
-  unsigned char etalon[8] = {0x82, 0, 1, 0x80, 0, 0, 0x2, 0};
-  unsigned char buf[8];
+  unsigned char etalon[sizeof(First)] = {0x82, 0, 1, 0x80, 0, 0, 0x2, 0};
+  unsigned char buf[sizeof(First)];
 
   // marshal test
-  err = Marshal_First(&c, buf, 8);
+  err = Marshal_First(&c, buf, sizeof(First));
   if (err == -1 ) {
     printf("FAIL! marshal\n");
     return 1;
   }
-  if (memcmp(etalon, buf, 8) != 0) {
+  if (memcmp(etalon, buf, sizeof(First)) != 0) {
     printf("FAIL! marshal check\n");
     for (i = 0; i < 8; ++i) printf("0x%hhX\n", buf[i]);
     return 1;
   }
 
   // unmarshal test
-  err = Unmarshal_First(&cc, buf, 8);
+  err = Unmarshal_First(&cc, buf, sizeof(First));
   if (err == -1 ) {
     printf("FAIL! unmarshal\n");
     return 1;
   }
-  if (memcmp(&cc, &cc_etalon, 8)) {
+  if (memcmp(&cc, &cc_etalon, sizeof(First))) {
     printf("FAIL! unmarshal check\n");
     printf("cc.i = %d\n", cc.i);
     printf("cc.j = %d\n", cc.j);
@@ -43,7 +43,7 @@ int main() {
   }
 
   // test test =)
-  if (isFirst(buf, 8) != 1) {
+  if (isFirst(buf, sizeof(First)) != 1) {
     printf("FAIL! test\n");
     return 1;
   }
