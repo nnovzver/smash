14f26ae7ed7d1fa014d00b9c72a6c3ac5727ce44
Vladimir Ruzanov <nnovzver@gmail.com>
2015-07-22 16:46:08 +0300

    Refactor C Test

--- tests/test.c
+++ tests/test.c
@@ -5,40 +5,40 @@
 int main() {
   int err;
   int i;
-  First c = {0, 0, 3, 4};
-  First cc;
-  First cc_etalon = {2, 4, 3, 4};
-  unsigned char etalon[sizeof(First)] = {0x82, 0, 1, 0x80, 0, 0, 0x2, 0};
+  First tomarshal = {0, 0, 3, 4};
+  First tounmarshal;
+  First tounmarshal_etalon = {2, 4, 3, 4};
+  unsigned char etalon_buf[sizeof(First)] = {0x82, 0, 1, 0x80, 0, 0, 0x2, 0};
   unsigned char buf[sizeof(First)];
 
   // marshal test
-  err = Marshal_First(&c, buf, sizeof(First));
+  err = Marshal_First(&tomarshal, buf, sizeof(First));
   if (err == -1 ) {
     printf("FAIL! marshal\n");
     return 1;
   }
-  if (memcmp(etalon, buf, sizeof(First)) != 0) {
+  if (memcmp(etalon_buf, buf, sizeof(First)) != 0) {
     printf("FAIL! marshal check\n");
     for (i = 0; i < 8; ++i) printf("0x%hhX\n", buf[i]);
     return 1;
   }
 
   // unmarshal test
-  err = Unmarshal_First(&cc, buf, sizeof(First));
+  err = Unmarshal_First(&tounmarshal, buf, sizeof(First));
   if (err == -1 ) {
     printf("FAIL! unmarshal\n");
     return 1;
   }
-  if (memcmp(&cc, &cc_etalon, sizeof(First))) {
+  if (memcmp(&tounmarshal, &tounmarshal_etalon, sizeof(First))) {
     printf("FAIL! unmarshal check\n");
-    printf("cc.i = %d\n", cc.i);
-    printf("cc.j = %d\n", cc.j);
-    printf("cc.k = %d\n", cc.k);
-    printf("cc.l = %d\n", cc.l);
-    printf("cc_etalon.i = %d\n", cc_etalon.i);
-    printf("cc_etalon.j = %d\n", cc_etalon.j);
-    printf("cc_etalon.k = %d\n", cc_etalon.k);
-    printf("cc_etalon.l = %d\n", cc_etalon.l);
+    printf("tounmarshal.i = %d\n", tounmarshal.i);
+    printf("tounmarshal.j = %d\n", tounmarshal.j);
+    printf("tounmarshal.k = %d\n", tounmarshal.k);
+    printf("tounmarshal.l = %d\n", tounmarshal.l);
+    printf("tounmarshal_etalon.i = %d\n", tounmarshal_etalon.i);
+    printf("tounmarshal_etalon.j = %d\n", tounmarshal_etalon.j);
+    printf("tounmarshal_etalon.k = %d\n", tounmarshal_etalon.k);
+    printf("tounmarshal_etalon.l = %d\n", tounmarshal_etalon.l);
     return 1;
   }
 
